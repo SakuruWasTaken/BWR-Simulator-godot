b541d696ff9450a811509b104b24e95c403ffb66
Gamerboi12345 <95109681+Gamerboi12345@users.noreply.github.com>
2023-07-07 18:22:15 -0700

    updated to 4.1, fixed some code in Main.gd

--- Assets/Scripts/Breaker Switch.gd
+++ Assets/Scripts/Breaker Switch.gd
@@ -9,6 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+<<<<<<< Updated upstream
 @onready var node_3d = $"/root/Node3d"
 @onready var electrical_system = $"/root/Node3d/Control Room Panels/Main Panel Right Side/Electrical System"
 
@@ -25,6 +26,66 @@ func electrical_breaker_switch(position, switch):
 	elif position == -1:
 		electrical_system.breakers[switch]["closed"] = false
 		electrical_system.breakers[switch]["auto_close_inhibit"] = true
+=======
+
+func cb_85_82_switch(position): print(position) # TODO
+func DG2_switch(position): print(position) # TODO
+func cb_DG2_8_switch(position): print(position) # TODO
+func cb_B8_switch(position): print(position) # TODO
+func cb_8DG2_switch(position): print(position) # TODO
+func cb_8_85_1_switch(position): print(position) # TODO
+func cb_8_83_switch(position): print(position) # TODO
+func cb_8_81_switch(position): print(position) # TODO
+func cb_8_3_switch(position): print(position) # TODO
+
+@onready var switches = {
+	"cb_85_82": {
+		"func": "cb_85_82_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"DG2": {
+		"func": "DG2_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_DG2_8": {
+		"func": "cb_DG2_8_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_B8": {
+		"func": "cb_B8_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_8DG2": {
+		"func": "cb_8DG2_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_8_85_1": {
+		"func": "cb_8_85_1_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_8_83": {
+		"func": "cb_8_83_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_8_81": {
+		"func": "cb_8_81_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+	"cb_8_3": {
+		"func": "cb_8_3_switch",
+		"position": 1,
+		"indicator": $"../Indicator".get_material()
+	},
+}
+>>>>>>> Stashed changes
 
 var positions = {
 	-1: 90,
@@ -37,6 +98,7 @@ func breaker_switch_position_up(camera, event, position, normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1:
 		var name = get_parent().name
+<<<<<<< Updated upstream
 		if name not in node_3d.breaker_switches:
 			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
 			return
@@ -54,11 +116,31 @@ func breaker_switch_position_up(camera, event, position, normal, shape_idx):
 			$"../Handle".set_rotation_degrees(Vector3(positions[node_3d.breaker_switches[name]["position"]], 0, 0))
 			if "func" in node_3d.breaker_switches[name]:
 				call(node_3d.breaker_switches[name]["func"], node_3d.breaker_switches[name]["position"], name)
+=======
+		if name not in switches:
+			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
+			return
+		if mouse_click.pressed:
+			if not switches[name]["position"] in [-1, 0]:
+				switches[name]["position"] -= 1
+				if switches[name]["position"] == 0:
+					switches[name]["indicator"].albedo_color = Color(0, 1, 0)
+				$"../AudioStreamPlayer3D".playing = true
+				$"../Handle".set_rotation_degrees(Vector3(positions[switches[name]["position"]], 0, 0))
+				call(switches[name]["func"], switches[name]["position"])
+		elif switches[name]["position"] == 0:
+			switches[name]["position"] = 1
+			$"../AudioStreamPlayer3D".playing = true
+			$"../Handle".set_rotation_degrees(Vector3(positions[switches[name]["position"]], 0, 0))
+			if "func" in switches[name]:
+				call(switches[name]["func"], switches[name]["position"])
+>>>>>>> Stashed changes
 
 func breaker_switch_position_down(camera, event, position, normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1:
 		var name = get_parent().name
+<<<<<<< Updated upstream
 		if name not in node_3d.breaker_switches:
 			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
 			return
@@ -76,11 +158,31 @@ func breaker_switch_position_down(camera, event, position, normal, shape_idx):
 			$"../Handle".set_rotation_degrees(Vector3(positions[node_3d.breaker_switches[name]["position"]], 0, 0))
 			if "func" in node_3d.breaker_switches[name]:
 				call(node_3d.breaker_switches[name]["func"], node_3d.breaker_switches[name]["position"], name)
+=======
+		if name not in switches:
+			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
+			return
+		if mouse_click.pressed:
+			if not switches[name]["position"] == 2:
+				switches[name]["position"] += 1
+				if switches[name]["position"] == 2:
+					switches[name]["indicator"].albedo_color = Color(1, 0, 0)
+				$"../AudioStreamPlayer3D".playing = true
+				$"../Handle".set_rotation_degrees(Vector3(positions[switches[name]["position"]], 0, 0))
+				call(switches[name]["func"], switches[name]["position"])
+		elif switches[name]["position"] in [2, 0]:
+			switches[name]["position"] = 1
+			$"../AudioStreamPlayer3D".playing = true
+			$"../Handle".set_rotation_degrees(Vector3(positions[switches[name]["position"]], 0, 0))
+			if "func" in switches[name]:
+				call(switches[name]["func"], switches[name]["position"])
+>>>>>>> Stashed changes
 
 func breaker_switch_position_lock(camera, event, position, normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1:
 		var name = get_parent().name
+<<<<<<< Updated upstream
 		if name not in node_3d.breaker_switches:
 			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
 			return
@@ -91,3 +193,15 @@ func breaker_switch_position_lock(camera, event, position, normal, shape_idx):
 				$"../AudioStreamPlayer3D".playing = true
 				$"../Handle".set_rotation_degrees(Vector3(positions[node_3d.breaker_switches[name]["position"]], 0, 0))
 				call(node_3d.breaker_switches[name]["func"], node_3d.breaker_switches[name]["position"], name)
+=======
+		if name not in switches:
+			print("WARNING: a switch with the name '%s' was turned in-game, but this switch was found in the code, the switch will do nothing." % [name])
+			return
+		if mouse_click.pressed:
+			if not switches[name]["position"] == -1:
+				switches[name]["indicator"].albedo_color = Color(0, 1, 0)
+				switches[name]["position"] = -1
+				$"../AudioStreamPlayer3D".playing = true
+				$"../Handle".set_rotation_degrees(Vector3(positions[switches[name]["position"]], 0, 0))
+				call(switches[name]["func"], switches[name]["position"])
+>>>>>>> Stashed changes
